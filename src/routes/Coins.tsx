fd453dcdb063d69f4b6e05dd2af7946a4e570761
tnsgud <qkrtnsgud0229@gmail.com>
2024-02-27 17:46:46 +0900

    test

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   padding: 0 20px;
+  margin: 0 auto;
+  max-width: 480px;
 `;
 const Header = styled.header`
   height: 10vh;
